426aadd69ba5e2f8df20520d2fca544636a5eaf3
Andy Feetenby <andyfeetenby@gmail.com>
2020-08-05 16:14:28 +0700

    Added searchBar.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div className="ui container" style={{ marginTop: '10px'}}>
+//       <SearchBar />
+//     </div>
+//   );
+// };
+
+class App extends React.Component  {
+  state = { images: [] };
+
+  onSearchSubmit(term){
+    // ajax async request to api 1st arg root url and endpoint search/photos
+    // 2nd argument configure request. Returns a promise(tap on shoulder)
+    // run .then function running an arrow function
+    axios.get('https://api.unsplash.com/photos/?client_id=4pcWvsVj29s8L97mSsV0XpMExWyXTpXEXBIpQYn7rZo',{
+      params: { query: term }
+    })
+    .then(response => {
+      this.setState({images: response.data});
+    });
+  }
+
+  render(){
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+      <SearchBar onSubmit={this.onSearchSubmit} />
+      Found: {this.state.images.length} images
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,17 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  // event handler, arrow function, solves problem with 'this' being undefined
+  onFormSubmit = (event) => {
+      event.preventDefault();
+      // from paren
+      this.props.onSubmit(this.state.term);
+  }
+
   render(){
     return(
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input type="text" value={this.state.term} onChange={ (e) => this.setState({ term: e.target.value })} />
